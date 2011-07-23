96b05df406703c7c27fac491186caad56f0db7ec
katsummy <katsummy@gmail.com>
2011-07-23 13:26:02 +0900

    init

---
+++ src/org/dpthon/BurgerTimer/TimerSetActivity.java
@@ -0,0 +1,14 @@
+package org.dpthon.BurgerTimer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TimerSetActivity extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.timerset);
+	}
+
+}
