d015c5bb52f5e9f5f5d5b00f87454946d41b1c8f
katsummy <katsummy@gmail.com>
2011-07-23 13:52:10 +0900

    画面遷移

--- src/org/dpthon/BurgerTimer/SplashActivity.java
+++ src/org/dpthon/BurgerTimer/SplashActivity.java
@@ -1,7 +1,9 @@
 package org.dpthon.BurgerTimer;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SplashActivity extends Activity {
 
@@ -9,5 +11,11 @@ public class SplashActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.splash);
+	}
+
+	public void onClick(View v) {
+		Intent intent = new Intent(getApplication(), TimerSetActivity.class);
+		startActivity(intent);
+		SplashActivity.this.finish();
 	}
 }
--- src/org/dpthon/BurgerTimer/TimerSetActivity.java
+++ src/org/dpthon/BurgerTimer/TimerSetActivity.java
@@ -1,14 +1,25 @@
 package org.dpthon.BurgerTimer;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 
 public class TimerSetActivity extends Activity {
+
+	private final Handler mHandler = new Handler();
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.timerset);
+	}
+
+	public void onClick(View v) {
+		Intent intent = new Intent(getApplication(), TimerActivity.class);
+		startActivity(intent);
+		TimerSetActivity.this.finish();
 	}
 
 }
